6906bb82a851a3379b96aa9cc92b8e7316efd1c7
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-07-20 13:38:15 +0545

    connected to mongoDB database

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+require('dotenv').config()
+
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(process.env.CONNECTION_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        })
+        console.log('Connection to database successful')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports=connectDB;
