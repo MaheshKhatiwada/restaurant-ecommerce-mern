c623c5c06995f1acc987adc077801f61721df350
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-07-26 08:58:20 +0545

    created add food modal

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -11,37 +11,34 @@ const AdminDashboard = () => {
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    setErrorMessage('');
-    setSuccessMessage('')
+    setErrorMessage("");
+    setSuccessMessage("");
     setCategory(e.target.value);
   };
 
-  const handleModalMessage=()=>{
-    setErrorMessage('')
-    setSuccessMessage('')
-
-  }
+  const handleModalMessage = () => {
+    setErrorMessage("");
+    setSuccessMessage("");
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (isEmpty(category)) {
-      setErrorMessage('Please enter a category');
+      setErrorMessage("Please enter a category");
     } else {
       setLoading(true);
       const data = { category };
       addCategory(data)
-      .then((response) =>{
-        setLoading(false)
-        setSuccessMessage(response.data.successMsg)
-        setCategory('')
-        }
-        )
-        .catch((error) =>
-        {
+        .then((response) => {
+          setLoading(false);
+          setSuccessMessage(response.data.successMsg);
+          setCategory("");
+        })
+        .catch((error) => {
           setLoading(false);
           setErrorMessage(error.response.data.errorMsg);
         });
-      }
     }
+  };
   const showHeader = () => (
     <div className="bg-dark text-white py-3 mx-2">
       <div className="container">
@@ -70,7 +67,11 @@ const AdminDashboard = () => {
             </button>
           </div>
           <div className="col-md-4 mb-2">
-            <button className="btn btn-outline-warning w-100">
+            <button
+              className="btn btn-outline-warning w-100"
+              data-bs-toggle="modal"
+              data-bs-target="#addFoodModal"
+            >
               <i className="fas fa-plus"> Add Food</i>
             </button>
           </div>
@@ -84,7 +85,7 @@ const AdminDashboard = () => {
     </div>
   );
   const showAddCategoryModal = () => (
-    <div id="addCategoryModal" className="modal"  onClick={handleModalMessage}>
+    <div id="addCategoryModal" className="modal" onClick={handleModalMessage}>
       <div className="modal-dialog modal-dialog-centered modal-lg">
         <div className="modal-content">
           <form onSubmit={handleSubmit}>
@@ -116,10 +117,57 @@ const AdminDashboard = () => {
               )}
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
                 Close
               </button>
               <button type="submit" className="btn btn-info">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+
+  const showFoodModal = () => (
+    <div id="addFoodModal" className="modal" onClick={handleModalMessage}>
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content">
+          <form onSubmit={handleSubmit}>
+            <div className="modal-header bg-warning text-white ">
+              <h5 className="modal-title ">Add Food</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body my-2">
+              {successMessage && showSuccessMessage(successMessage)}
+              {errorMessage && showErrorMessage(errorMessage)}
+              {loading ? (
+                <div className="text-center">{showLoading()}</div>
+              ) : (
+                <>
+
+                </>
+              )}
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" className="btn btn-warning text-white">
                 Submit
               </button>
             </div>
@@ -133,6 +181,7 @@ const AdminDashboard = () => {
       {showHeader()}
       {showActionBtns()}
       {showAddCategoryModal()}
+      {showFoodModal()}
     </section>
   );
 };
