49956fb2a847f7d53222911b25baa1060a0f1f2a
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-01 09:47:10 +0545

    removed morgan

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const morgan = require("morgan");
+//const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./database/db");
 const authRoutes = require("./routes/auth");
@@ -11,7 +11,7 @@ const path=require('path')
 
 //middleware
 app.use(cors());
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
